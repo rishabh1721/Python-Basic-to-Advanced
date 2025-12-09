01b52e66877044e475d44c4e2b54bea3f5d988c7
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-09 17:14:20 +0530

    day-one-calculator-built

---
+++ Day-1/Project-1/simple_calculator.py
@@ -0,0 +1,25 @@
+#Learn about Input and Data Conversion
+
+str_num_a = input('Enter First Number: ')
+str_num_b = input('Enter Second Number: ')
+
+num_a = float(str_num_a)
+num_b = float(str_num_b)
+
+# print(type(num_a))
+
+total = num_a + num_b
+print(total)
+
+
+#DataTYpes functions
+
+x=10
+str_x = str(x)
+float_x = float(str_x)
+print(float_x)
+bool_x = bool(str_x)
+print(bool_x)
+str_x = str(str_x)
+float_x = float(str_x)
+print(float_x)

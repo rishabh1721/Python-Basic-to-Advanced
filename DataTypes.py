671cfcc8c87042ccdc0240b30167242982fe7aac
rishabh1721 <rishabhmcu001@gmail.com>
2025-12-09 16:57:41 +0530

    day-one-data-types-strings

---
+++ DataTypes.py
@@ -0,0 +1,42 @@
+#Basic data types in pyhton
+#-------------------------------
+
+string = 'text'
+str_number = 'number'
+num_int = 10
+num_float = 0.1
+boolean = True
+none_type = None
+
+
+print(type(num_int))
+print(type(str_number))
+print(type(num_float))
+print(type(boolean))
+print(type(none_type))
+
+
+#Simple Exercise
+#-------------------------------
+
+user = 'student'
+app =  'python'
+print(type(user))
+print(type(app))
+
+print(f"Once there was a curious {user} who wanted to become an AI expert.")
+print(f"Every night, they practiced {app} until the laptop begged for rest.")
+print(f"One day, they discovered a bug so strange even StackOverflow got confused.")
+print(f"After hours of struggle, the student finally fixed it with a single missing comma.")
+print(f"That moment, they realized debugging is just detective work with extra pain.")
+print(f"And from that day, they coded smarter, not harder—legend in the making.")
+
+#string formatting
+
+print(f'My name is: {user}')
+
+
+
+
+
+
